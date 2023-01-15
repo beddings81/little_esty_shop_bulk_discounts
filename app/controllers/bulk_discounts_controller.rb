3af88ae5e67528d0e0567d9cdcf0b4b9076da7c3
Braxton Eddings <beddings81@gmail.com>
2023-01-15 03:43:33 -0600

    Pass test for a from to update a bulk discount in the edit page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -29,7 +29,20 @@ class BulkDiscountsController < ApplicationController
   def edit
     @bulk_discount = BulkDiscount.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
+  end
 
-    # redirect_to edit_merchant_bulk_discount_path(@merchant, @bulk_discount)
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount.update(bulk_discount_params)
+
+    redirect_to merchant_bulk_discount_path(@merchant, @bulk_discount)
+  end
+
+
+  private
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:percent_discount, :quantity_threshold)
   end
 end
