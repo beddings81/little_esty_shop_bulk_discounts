55ccdbda76d003a300d6aebf48fc58049b81bf9b
Braxton Eddings <beddings81@gmail.com>
2023-01-16 12:02:52 -0600

    Pass tests for #discount and #discounted_total_revenue

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,6 +1,6 @@
 class BulkDiscount < ApplicationRecord
   validates_presence_of :percent_discount, :quantity_threshold
-  validates :percent_discount, numericality: { only_integer: true }
+  validates :percent_discount, numericality: { only_integer: false }
   validates :quantity_threshold, numericality: { only_integer: true }
   belongs_to :merchant
 end
